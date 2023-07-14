ce6025dd32c05d6ac8d7cff48d52d297308f5845
Vanessa Silva Ferreira <111510477+VanessaSFerreira@users.noreply.github.com>
2023-07-14 09:23:11 -0300

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#create a folder "data" in default dataset
+#create a folder "data" in your working directory
 if(!file.exists("./data")){dir.create("./data")}
 
 #save the web path in a variable called fileUrl 
@@ -19,42 +19,55 @@ unzip(zipF,exdir=outDir)
 #reading files and storing in variables
 dataTrain <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/train/X_train.txt")
 labelTrain <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/train/y_train.txt")
+subjectTrain <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/train/subject_train.txt")
 dataTest <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/test/X_test.txt")
 labelTest <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/test/y_test.txt")
+subjectTest <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/test/subject_test.txt")
 features <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/features.txt")
 activity_labels <- read.table("./data/UCI_HAR_Dataset/UCI HAR Dataset/activity_labels.txt")
 
 #loading necessary library
 library(dplyr)
 
-#to combine columns activity id and collected data
-dataTrain <- cbind(labelTrain,dataTrain)
-dataTest <- cbind(labelTest,dataTest)
+#to combine columns subject, activity id and collected data
+dataTrain <- cbind(subjectTrain,labelTrain,dataTrain)
+dataTest <- cbind(subjectTest,labelTest,dataTest)
 
 #to combine both datasets (train and test) in order to create just one
 HAR <- rbind(dataTrain,dataTest)
 
 #to change the name of the columns, using the features
-names(HAR)<- c("label", features$V2)
+names(HAR)<- c("subject","activity", features$V2)
 
 #just cleaning some data in the memory
-rm(dataTest,dataTrain,features,labelTest,labelTrain)
+rm(dataTest,dataTrain,features,labelTest,labelTrain,subjectTest,subjectTrain)
 
 #to select just variables related to mean and std
 #to join the description of the activitieS
 #to exclude the id activity in order to leave just the description as the first column
-HAR_activity_labels <- HAR %>% select(label,grep("mean()|std()", names(HAR))) %>%
-                        merge(activity_labels,by.x="label",by.y="V1",all=TRUE) %>% 
-                        select(V2, 2:80);
+TidyData <- HAR %>% select(subject,activity,grep("mean|std", names(HAR))) %>%
+        merge(activity_labels,by.x="activity",by.y="V1",all=TRUE) %>%
+        select(V2, 2:81);
 
-#to change the name of the first column to activity
-names(HAR_activity_labels)[1] <- c("activity")
+#label data set with descriptive variable names using gsub
+names(TidyData)[1] = "activity"
+names(TidyData) <- gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData) <- gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData) <- gsub("BodyBody", "Body", names(TidyData))
+names(TidyData) <- gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData) <- gsub("^t", "Time", names(TidyData))
+names(TidyData) <- gsub("^f", "Frequency", names(TidyData))
+names(TidyData) <- gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData) <- gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("angle", "Angle", names(TidyData))
+names(TidyData) <- gsub("gravity", "Gravity", names(TidyData))
 
 #to group data by activity 
 #to create a tidy data with the mean by activity
-HAR_avg_byactivity <- HAR_activity_labels %>%
-                        group_by(activity) %>%
-                        summarise_each(funs(mean))
+HAR_avg_byactivity <- TidyData %>%
+        group_by(subject,activity) %>%
+        summarise_each(funs(mean))
 
-write.csv(HAR_activity_labels,file="./data/HAR_tidydata.csv", row.names=TRUE)
-write.csv(HAR_avg_byactivity,file="./data/HAR_avgbyactivity.csv", row.names=TRUE)
+write.table(HAR_avg_byactivity,file="./data/tidyData.txt", row.names=FALSE)
